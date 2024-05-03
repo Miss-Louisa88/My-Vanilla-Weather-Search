beaa0dba5bfc228190de69b78494290985ec9e36
Miss-Louisa88 <167814167+Miss-Louisa88@users.noreply.github.com>
2024-05-03 20:03:58 +0300

    Added GitHub axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert("Louise is a rich developer");
+function searchCity(event) {
+  event.preventDefault();
+  let searchResult = document.querySelector("#enterCity");
+  console.log(searchResult);
+  let cityElement = document.querySelector("h1");
+  console.log(cityElement);
+  cityElement.innerHTML = searchResult.value;
+}
+let form = document.querySelector("#city");
+form.addEventListener("submit", searchCity);
