91028626187fd83d609b4d7821e10fbe79421618
Miss-Louisa88 <167814167+Miss-Louisa88@users.noreply.github.com>
2024-05-04 01:09:20 +0300

    API integration
    Added some real-time data

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ function searchCity(event) {
     );
     let currentWeather = document.querySelector("#icon");
     currentWeather.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon"/>`;
+    let weatherDescription = document.querySelector("#description");
+    weatherDescription.innerHTML = response.data.condition.description;
+    let currentHumidity = document.querySelector("#weatherUpdate");
+    currentHumidity.innerHTML = response.data.temperature.humidity;
+    let currentWind = document.querySelector("#wind-speed");
+    currentWind.innerHTML = `response.data.wind.speed ${Km / hr}`;
   }
   let apiKey = "594o02104b870cb39413fdbd5t9a24d2";
   let city = `${searchResult.value}`;
@@ -22,3 +28,20 @@ function searchCity(event) {
 }
 let form = document.querySelector("#city");
 form.addEventListener("submit", searchCity);
+
+///Date API Integration
+function createDate() {
+  let now = new Date();
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  let day = days[now.getDay()];
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+}
