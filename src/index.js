9f2e4d1cc984f4eaf3b6e3d835e81c08caf19015
Miss-Louisa88 <167814167+Miss-Louisa88@users.noreply.github.com>
2024-05-04 23:17:31 +0300

    Added real time data

--- src/index.js
+++ src/index.js
@@ -18,8 +18,31 @@ function searchCity(event) {
     let currentHumidity = document.querySelector("#weatherUpdate");
     currentHumidity.innerHTML = response.data.temperature.humidity;
     let currentWind = document.querySelector("#wind-speed");
-    currentWind.innerHTML = `response.data.wind.speed ${Km / hr}`;
+    currentWind.innerHTML = `${response.data.wind.speed} Km / hr`;
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    console.log(date);
+    timeElement.innerHTML = formatDate(date);
   }
+  function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}: ${minutes}`;
+  }
+
   let apiKey = "594o02104b870cb39413fdbd5t9a24d2";
   let city = `${searchResult.value}`;
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
